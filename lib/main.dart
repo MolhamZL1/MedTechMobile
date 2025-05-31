d93df6c981d4f055ed6690e1b47b0a356db6f059
molhamsheikh <molhamsa49@gmail.com>
2025-05-31 20:33:07 +0300

    creating project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MedTech());
+}
+
+class MedTech extends StatelessWidget {
+  const MedTech({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
