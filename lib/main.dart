c11a72322933df7a7388b135456a89884d12c43b
molhamsheikh <molhamsa49@gmail.com>
2025-06-04 12:37:58 +0300

    add login ui

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MedTech extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SignInView());
+    return const MaterialApp(home: Center(child: SignInView()));
   }
 }
